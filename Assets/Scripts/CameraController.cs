999b533de13f57d8cdef0f36e4c9162a79d87e2b
FabioBrigidi <fabio.brigidi@gmail.com>
2017-12-24 15:19:54 +0100

    First commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Transform charTransform;
+    public Vector3 offset;
+
+    public float xMin, xMax, yMin, yMax;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        Vector3 position = new Vector3(Mathf.Clamp(charTransform.position.x, xMin, xMax), Mathf.Clamp(charTransform.position.y, yMin, yMax), charTransform.position.z);
+        transform.position = position + offset;
+
+	}
+
+    
+}
